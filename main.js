8c30253f2d05cd42aa6b570f5fd61dd3e510064e
Sara Anita <sara.anita.desa@gmail.com>
2024-09-12 20:03:40 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+/* 1º Desafio */
+
+document.querySelector('input[type=tel]');
+
+/* 2º Desafio */
+
+document.querySelector('.tecla_pom');
+
+/* 3º Desafio */
+
+/* <button>Clique aqui</button>
+<script src="main.js"></script> */
+
+let botao = document.querySelector('button');
+
+botao.addEventListener('click', function() {
+    alert('Fui clicado!');
+});
+
+/* 4º Desafio */
+
+/* .meuPlayer {
+    background-color: #fff;
+    border-radius: 5px;
+    color: #000;
+    /* Adicione mais estilos conforme necessário */
---
+++ read-me.md
@@ -0,0 +1,3 @@
+Nesse Primeiro desafio eu tinha de:
+Escreva um código em JavaScript capaz de selecionar o elemento HTML input do tipo telefone:
+
